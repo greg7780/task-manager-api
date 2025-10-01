875e927933eacd458499798d2c5d541e78ef98d0
Gregory Wilson Wijaya <gregory7780@gmail.com>
2025-10-01 10:57:10 +0700

    Setup project

---
+++ TaskManagerApi/Data/TaskManagerContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagerApi.Data
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext(DbContextOptions<TaskManagerContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ TaskManagerApi/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagerApi.Models
+{
+    public class Task
+    {
+        public long Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public int Priority { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
